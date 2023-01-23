53dfb7bcb0aacbbf7bd2538badf7e20d0e4e753f
Brian <wamwangimathenge@gmail.com>
2023-01-23 12:09:26 +0300

    GifList Container Component Added

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,21 @@
 import React from "react";
 
 import NavBar from "./NavBar";
+import GifListContainer from "./GifListContainer";
+
 
 // the App component should render out the GifListContainer component
 
 function App() {
   return (
+    <>
     <div>
       <NavBar color="black" title="Giphy Search" />
     </div>
+    <div>
+      <GifListContainer />
+    </div>
+    </>
   );
 }
 
---
+++ src/components/GifListContainer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GifListContainer = () => {
+    return (  );
+}
+ 
+export default GifListContainer;
