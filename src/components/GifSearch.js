624c67f964b908cf0a720ee6d0a1bf6f2154fbd8
Brian <wamwangimathenge@gmail.com>
2023-01-23 12:28:02 +0300

    Search Component Updated

--- src/components/GifSearch.js
+++ src/components/GifSearch.js
@@ -1,6 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+
+
+function GifSearch({fetchData}) {
+    const [search, setSearch] = useState("")
+    
+
+    function handleInputChange(event) {
+        setSearch(() => event.target.value)
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        fetchData(searchTerm)
+    }
 
 
 
+    return(
+        <div>
+            <h2>GifSearch</h2>
+            <form onSubmit={handleSubmit}>
+                <label for="search">Search</label>
+                <br></br>
+                <input type="text" name="search" onChange={handleInputChange} />
+                <br></br>
+                <button type="submit">Search</button>
+            </form>
+        </div>
+    )
+}
  
 export default GifSearch;
